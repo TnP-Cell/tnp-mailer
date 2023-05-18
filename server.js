8cfa059ea0024207b569f6af32a137e8afb4a90a
prateekch33 <prateekchandra27@outlook.com>
2023-05-18 16:37:34 +0530

    registeration mailing starts

--- server.js
+++ server.js
@@ -24,6 +24,105 @@ app.post(`/sendMail`, async (req, res) => {
   return res.status(200).json(check);
 });
 
+app.post("/registermail", async (req, res) => {
+  let check = await registration(req.body);
+  if (check.status === -1) return res.status(400).json(check);
+  return res.status(200).json(check);
+});
+
+const registration = async (data) => {
+  let mailOptions = {
+    from: `${process.env.USER}`,
+    to: `${data.username}`,
+    subject: `Login Credentials || TnP IIITBH`,
+    html: `<h1 style="color: #f3b300; text-decoration: underline; font-weight: 700">
+    Training and Placement Website Credentials
+  </h1>
+  
+  <p style="font-size: 18px">
+    This credentials belongs to the admin section of the Training and Placement
+    Cell Website. Using these credentials you can able to login and see which companies have registered
+    for the recruitment drive.
+  </p>
+  
+  <table style="width: 40%">
+    <tr style="width: 100%">
+      <th
+        style="
+          border: 2px solid #f3b300;
+          width: 60%;
+          padding: 2%;
+          border-radius: 10px;
+          text-align: center;
+        "
+      >
+        Username
+      </th>
+      <th
+        style="
+          border: 2px solid #f3b300;
+          width: 40%;
+          padding: 2%;
+          border-radius: 10px;
+          text-align: center;
+        "
+      >
+        Password
+      </th>
+    </tr>
+    <tr>
+      <td
+        style="
+          border: 2px solid #f3b300;
+          width: 60%;
+          padding: 2%;
+          border-radius: 10px;
+          text-align: center;
+        "
+      >
+        ${data.username}
+      </td>
+      <td
+        style="
+          border: 2px solid #f3b300;
+          width: 60%;
+          padding: 2%;
+          border-radius: 10px;
+          text-align: center;
+        "
+      >
+        ${data.password}
+      </td>
+    </tr>
+  </table>
+  
+  <p style="font-size: 18px">
+    Go through the given link to login to your account:
+    <a href="https://tnp.iiitbh.ac.in/admin">https://tnp.iiitbh.ac.in/admin</a>. Kindly do login and use this portal only through your PC/Laptop.
+  </p>
+
+  <span>--</span>
+  <h5 style="line-height: 1px">Thanks & Regards,</h5>
+  <h5 style="line-height: 1px">Website Development Team,</h5>
+  <h5 style="line-height: 1px">Training and Placement Cell</h5>
+  <h5 style="line-height: 20px">
+    <span style="color: blueviolet">For any query, Contact:</span>
+    <a href="tel:+919431796788" style="color: #878787">+91 9431796788</a>,
+    <a href="mailto:tnp@iiitbh.ac.in" style="color: #878787">tnp@iiitbh.ac.in</a>
+  </h5>
+  `,
+  };
+  await transporter
+    .sendMail(mailOptions)
+    .then((info) => {
+      console.log("Message sent: %s", info.messageId);
+    })
+    .catch((err) => {
+      return { status: -1, error: err.message };
+    });
+  return { status: 0 };
+};
+
 const sender = async (data) => {
   let mailOptions = {
     from: process.env.USER,
